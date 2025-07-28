0d13ccca22e6b10438bbee2c0e8acec4a15af451
Weslley Trindade <weslleyatr07@gmail.com>
2025-07-28 18:43:51 -0300

    aulas datetime

---
+++ Data-Hora-Fuso/1_datetime.py
@@ -0,0 +1,12 @@
+from datetime import datetime, date, time
+
+data = date(2024, 7, 28)
+
+print(data)
+print(date.today())
+
+data_hora = datetime.now()
+print(data_hora)
+
+hora = time(12, 30, 45)
+print(hora)
